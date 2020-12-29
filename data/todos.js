7cf6595021bc6f84428bb2085e76f5a5ad4bc062
Fortis-Potens <fortispotens@gmail.com>
2020-12-29 14:37:54 +0100

    Setup unit testing with Mocha and Chai

--- data/todos.js
+++ data/todos.js
@@ -2,17 +2,14 @@ const todos = [
 	{
 		name: 'Wake up',
 		isCompleted: false,
-		createdAt: new Date().toUTCString(),
 	},
 	{
 		name: 'Take my bath',
 		isCompleted: false,
-		createdAt: new Date().toUTCString(),
 	},
 	{
 		name: 'Get ready for work',
 		isCompleted: false,
-		createdAt: new Date().toUTCString(),
 	},
 ];
 
---
+++ src/test/app.spec.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../config/config.env' });
+
+import app from '../app.js';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+const appURL = '/api/v1';
+
+describe('Todo API Server', () => {
+	it('Welcomes user to the Todo API Application', (done) => {
+		chai
+			.request(app)
+			.get('/')
+			.end((error, response) => {
+				expect(response.status).to.eq(200);
+				expect(response.body.success).to.equals(true);
+				expect(response.body.message).to.equal('Welcome to our Todo API');
+				done();
+			});
+	});
+});
